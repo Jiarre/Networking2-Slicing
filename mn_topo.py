6fad7c7ca2981311fc8dbc4d5e631c80eb61cf60
Federico Giarrè <38040300+Jiarre@users.noreply.github.com>
2021-12-27 12:22:32 +0100

    Added sftp and raspi bridge

--- mn_topo.py
+++ mn_topo.py
@@ -12,6 +12,8 @@ from mininet.topo import Topo
 from mininet.node import OVSKernelSwitch, RemoteController, Node
 from mininet.term import makeTerm
 from mininet.link import TCLink
+from comnetsemu.net import Containernet
+
 
 class LinuxRouter(Node):
     def config(self, **params):
@@ -25,29 +27,31 @@ class LinuxRouter(Node):
 class Topology(Topo):
 
     def build(self):
-        r1 = self.addHost("r1",cls=LinuxRouter,ip="192.168.1.1/24")
-        r2 = self.addHost("r2",cls=LinuxRouter,ip="192.168.2.1/24")
+        
+        h11 = self.addHost("it1", ip="192.168.2.73/24",defaultRoute='via 192.168.2.1',mac='00:00:00:00:00:0c')
+        h12 = self.addHost("it2", ip="192.168.2.74/24",defaultRoute='via 192.168.2.1',mac='00:00:00:00:00:0d')
 
+        h1 = self.addHost("a3", ip="192.168.2.21/24",defaultRoute='via 192.168.2.1',mac='00:00:00:00:00:01')
+        h2 = self.addHost("a4", ip="192.168.2.41/24",defaultRoute='via 192.168.2.1',mac='00:00:00:00:00:02')
+        h3 = self.addHost("h1", ip="192.168.2.22/24",defaultRoute='via 192.168.2.1',mac='00:00:00:00:00:03')
+        h4 = self.addHost("h2", ip="192.168.2.31/24",defaultRoute='via 192.168.2.1',mac='00:00:00:00:00:04')
+        h5 = self.addHost("h3", ip="192.168.2.32/24",defaultRoute='via 192.168.2.1',mac='00:00:00:00:00:05')
+        h6 = self.addHost("h4", ip="192.168.2.42/24",defaultRoute='via 192.168.2.1',mac='00:00:00:00:00:06')
+        h7 = self.addHost("h5", ip="192.168.2.51/24",defaultRoute='via 192.168.2.1',mac='00:00:00:00:00:07')
+        h8 = self.addHost("h6", ip="192.168.2.52/24",defaultRoute='via 192.168.2.1',mac='00:00:00:00:00:08')
 
-        h1 = self.addHost("a3", ip="192.168.1.21/24",defaultRoute='via 192.168.1.1')
-        h2 = self.addHost("a4", ip="192.168.1.41/24",defaultRoute='via 192.168.1.1')
-        h3 = self.addHost("h1", ip="192.168.1.22/24",defaultRoute='via 192.168.1.1')
-        h4 = self.addHost("h2", ip="192.168.1.31/24",defaultRoute='via 192.168.1.1')
-        h5 = self.addHost("h3", ip="192.168.1.32/24",defaultRoute='via 192.168.1.1')
-        h6 = self.addHost("h4", ip="192.168.1.42/24",defaultRoute='via 192.168.1.1')
-        h7 = self.addHost("h5", ip="192.168.1.51/24",defaultRoute='via 192.168.1.1')
-        h8 = self.addHost("h6", ip="192.168.1.52/24",defaultRoute='via 192.168.1.1')
+        h9 = self.addHost("a1", ip="192.168.3.61/24",defaultRoute='via 192.168.3.1',mac='00:00:00:00:00:09')
+        h10 = self.addHost("a2", ip="192.168.3.62/24",defaultRoute='via 192.168.3.1',mac='00:00:00:00:00:0a')
 
-        h9 = self.addHost("a1", ip="192.168.2.61/24",defaultRoute='via 192.168.2.1')
-        h10 = self.addHost("a2", ip="192.168.2.62/24",defaultRoute='via 192.168.2.1')
-
-        h11 = self.addHost("it1", ip="192.168.1.73/24",defaultRoute='via 192.168.1.1')
-        h12 = self.addHost("it2", ip="192.168.1.74/24",defaultRoute='via 192.168.1.1')
-
-        h13 = self.addHost("ftp", ip="192.168.1.75/24",defaultRoute='via 192.168.1.1')
         
 
-        #prova1 = self.addHost("p1",ip="192.168.2.21/24")
+        h13 = self.addHost("sftp", ip="192.168.2.75/24",defaultRoute='via 192.168.2.1',mac='00:00:00:00:00:0b')
+
+        r1 = self.addHost("r1",cls=LinuxRouter,ip="192.168.2.1/24",mac='00:00:00:00:00:0e')
+        r2 = self.addHost("r2",cls=LinuxRouter,ip="192.168.3.1/24",mac='00:00:00:00:00:0f')
+        
+        
+        #prova1 = self.addHost("p1",ip="192.168.2.22/24")
         #prova2 = self.addHost("p2",ip="192.168.2.22/24")
 
 
@@ -78,10 +82,11 @@ class Topology(Topo):
 
         self.addLink("s7","it1")
         self.addLink("s7","it2")
-        self.addLink("s7","ftp")
+        self.addLink("s7","sftp")
         
-        self.addLink("s1","r1",intfName2='r1-eth1',params2={'ip':'192.168.1.1/24'})
-        self.addLink("s6","r2",intfName2='r2-eth1',params2={'ip':'192.168.2.1/24'})
+        
+        self.addLink("s1","r1",intfName2='r1-eth1',params2={'ip':'192.168.2.1/24'})
+        self.addLink("s6","r2",intfName2='r2-eth1',params2={'ip':'192.168.3.1/24'})
         self.addLink("s1","s7")
 
         self.addLink(r1,
@@ -109,15 +114,26 @@ def runTopo():
     net.addController(controller)
     
     net.start()
-    net['r1'].cmd("sudo ip route add 192.168.2.0/24 via 10.100.0.2 dev r1-eth2")
-    net['r2'].cmd("sudo ip route add 192.168.1.0/24 via 10.100.0.1 dev r2-eth2")
-    net['a1'].cmd("iperf -s -p 21 &")
-    net['a2'].cmd("iperf -s -p 21 &")
-    net['a3'].cmd("iperf -s -p 21 &")
-    net['a4'].cmd("iperf -s -p 21 &")
-    net['ftp'].cmd("iperf -s -p 21 &")
-    for h in net.hosts:
-        h.cmd("iperf -s -p 5060 -u &")
+    n = net.getNodeByName("s7")
+    print("*** Setting up bridge network")
+    time.sleep(0.1)
+    n.cmd('sudo ovs-vsctl add-port s7 eth1')
+    net['r1'].cmd("sudo ip route add 192.168.3.0/24 via 10.100.0.2 dev r1-eth2")
+    net['r2'].cmd("sudo ip route add 192.168.2.0/24 via 10.100.0.1 dev r2-eth2")
+
+    
+    net['sftp'].cmd("python3 sshsocket.py &")
+    time.sleep(0.3)
+    voip = [net['h1'],net['h2'],net['h3'],net['h4'],net['h5'],net['h6'],net['a1'],net['a2'],net['a3'],net['a4'],net['it1'],net['it2']]
+    for h in voip:
+        h.cmd("python3 voipsocket.py &")
+        time.sleep(0.1)
+        print(f"Started {str(h)} socket")
+
+
+
+    print("All socket started")
+    n.cmd("hostname comnetsemu")
     
     CLI(net)
 
